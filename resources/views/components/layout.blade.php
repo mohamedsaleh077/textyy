09dcc814d3e454f79c7921b489774c5a172118d4
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-06-04 21:37:20 +0300

    first commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ isset($title) ? $title . ' - Textyy' : 'Textyy' }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body style="background-color: #d6d6d6">
+    <main class="container-md bg-light">
+        <nav class="row p-2 align-items-center bg-primary-subtle">
+            <div class="col fs-2 fw-bolder">
+                Textyy
+            </div>
+            <div class="col-md-auto row">
+                <div class="col align-middle align-self-center">
+                    username
+                </div>
+                <div class="col-auto">
+                    <a href="/login"><button class="btn btn-primary">Login</button></a>
+                    <a href="/register"><button class="btn btn-secondary">Signup</button></a>
+                    <a href="/logout"><button class="btn btn-primary">Logout</button></a>
+                </div>
+            </div>
+        </nav>
+        <article class="p-2">
+            {{ $slot }}
+        </article>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"
+        integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <x-slot:title>
+        Home Page
+    </x-slot:title>
+    <h1>Welcome To Textyy!</h1>
+    <p>Textyy is a platform to post any text you want, just register with any nickname you would like and post whatever
+        you want!</p>
+    <p>Be nice and respectful! leave a great smell from your text here!</p>
+    <p>have an account? <a href="/login">Login</a> or you can <a href="/register">join us</a> if you didn't yet!</p>
+</x-layout>
